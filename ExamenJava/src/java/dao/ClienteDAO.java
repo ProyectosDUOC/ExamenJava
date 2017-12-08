10e30dd2d5e8e3eb78c0a6450fbc9eab81f08a0c
Benja <benja.mora.torres@gmail.com>
2017-12-08 20:57:46 -0300

    Crud 1

--- ExamenJava/src/java/dao/ClienteDAO.java
+++ ExamenJava/src/java/dao/ClienteDAO.java
@@ -5,10 +5,75 @@
  */
 package dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import modelo.Cliente;
+
 /**
  *
  * @author benja
  */
 public class ClienteDAO {
+
+    private Cliente p;
+    private EntityManager em;
+
+    EntityManagerFactory factory = Persistence.createEntityManagerFactory("cliente", System.getProperties());
+
+    public boolean Crear(Cliente cliente) {
+        em = factory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(cliente);
+            em.flush();
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception ex) {
+            em.getTransaction().rollback();
+            return false;
+        }
+    }
+
+    public boolean Leer(String rut) {
+        p = em.find(Cliente.class, rut);
+
+        if (p.getRutCliente().isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean Eliminar(String rut) {
+        try{
+            p = em.find(Cliente.class, rut);
+            em.remove(p);
+            em.flush();
+            em.getTransaction().commit();
+            return true;
+        }
+        catch(Exception ex){
+            return false;
+        }        
+    }
     
+    public List<Cliente> Listar(){
+        
+        List<Cliente> clientes;
+        javax.persistence.Query q = em.createQuery("SELECT c FROM Cliente c");
+        clientes = q.getResultList();
+        int num_clientes = clientes.size();
+        
+        return clientes;
+    }
+    /*
+         if (opcion.equals("Listar")) {
+                for (Persona obj : personas) {
+                    out.println("<h2>" + obj.toString() + "<h2>");
+                }
+            }
+    
+    */
 }
