855ef314a820d02c571be0a757eda2190ebe0f96
Benja <benja.mora.torres@gmail.com>
2017-12-18 00:39:52 -0300

    Todo ACA

--- ExamenDEJ/src/java/dao/DetalleBoletaDAO.java
+++ ExamenDEJ/src/java/dao/DetalleBoletaDAO.java
@@ -7,6 +7,8 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
